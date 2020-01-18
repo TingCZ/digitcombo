bb8f5e63e52f40cf4dd33be0bf87795b70473c7f
TingCZ <Tingcheung1111@outlook.com>
2020-01-18 16:46:45 +0800

    Revise the name of profile

--- src/test/java/com/phonedigit/digitcombo/DigitComboApplicationTests.java
+++ src/test/java/com/phonedigit/digitcombo/DigitComboApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("test")
 class DigitComboApplicationTests{
-
     @Autowired
     CharCombiner charCombiner;
 
